88ef5c63079f98406df22d811a7b1fc54a665ee1
Rodrigo <rodluna97@gmail.com>
2022-01-12 18:29:08 -0300

    inserindo dados no banco e dashboard

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <p>Componente de Mensagens</p>
+        <Message :msg="msg" v-show="msg" />
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
 
             <div class="input-container">
                 <label for="nome">Nome do cliente:</label>
@@ -45,6 +45,8 @@
 </template>
 
 <script>
+import Message from './Message.vue'
+
 export default {
     name: "BurgerForm",
     data(){
@@ -56,7 +58,6 @@ export default {
         pao: null, 
         carne: null,
         opcionais: [],
-        status: "Solicitado",
         msg: null
       }
     },
@@ -68,10 +69,41 @@ export default {
         this.paes = data.paes;
         this.carnes = data.carnes;
         this.opcionaisdata = data.opcionais;
+      },
+
+       async createBurger(e) {
+      e.preventDefault()
+      const data = {
+        nome: this.nome,
+        carne: this.carne,
+        pao: this.pao,
+        opcionais: Array.from(this.opcionais),
+        status: "Solicitado"
+      }
+      const dataJson = JSON.stringify(data)    
+      const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type" : "application/json" },
+        body: dataJson
+      });
+      const res = await req.json();
+
+  
+      // colocar uma msg de sistema
+      this.msg = `Pedido N° ${res.id} realizado com sucesso`
+
+      //limpar msg
+      setTimeout(() => this.msg = "", 3000);
+      
+      // limpar os campos
+
       }
     },
     mounted(){
       this.getIngredientes()
+    },
+    components: {
+      Message
     }
 }
 </script>
